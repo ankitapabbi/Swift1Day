234a5cc5dab54f1797a4ca7ed46eec05ac079751
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-06 21:03:40 -0400

    Basic Operations.

---
+++ Swift1Day/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  Swift1Day
+//
+//  Created by MacStudent on 2019-06-06.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+let 🤞🏻="hello"
+print(🤞🏻)
+
+print(Int.max, Int.min)
+
+var a = 10;
+var b = 3;
+
+var sum = a+b;
+var sub = a-b;
+var mul = a*b;
+var div = a/b;
+var mod = a%b;
+print("Addition is :\(sum)");
+print("Subtraction is :\(sub)");
+print("Multipication is :\(mul)");
+print("Division is :\(div)");
+print("Mod is :\(mod)");
+
